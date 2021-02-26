0689f4c19202681eb974875ddfc4e834a64734b6
Roberto Garcia <Roberto_garcia87@hotmail.com>
2021-02-26 23:51:53 +0000

    login function working finally...

--- npm/src/Controllers/PasswordCont.js
+++ npm/src/Controllers/PasswordCont.js
@@ -10,6 +10,7 @@ const data ={
     password: '8737'
 }
 
+//POST ROUTES
 
 const Register = (req,res) => {
     
@@ -35,6 +36,32 @@ const Register = (req,res) => {
 }
 
 const Login = (req,res) => {
+
+    // sqlquery 
+  
+        const sql = `select * FROM users WHERE email='${data.email}'`
+         
+        connection.query(sql,(err,result) => {
+
+            // if array or sql result is empty
+            
+            if(result.length > 0 ) {
+
+                
+                // check passwords from the db
+                if(bcrypt.compareSync(data.password , result[0].password)){
+
+                    console.log('welcome')
+                }else{
+
+                    console.log('pasword or email incorrect')
+                }
+            }else{
+
+                console.log('email or password incorrect')
+            }
+        })
+    
 
 }
 
